5c8fb30652ebfacdfda168f22aee357cacd91e29
Bruno Oliveira <nicoddemus@gmail.com>
2018-02-05 21:03:22 -0200

    Spawn another task and reduce move_on_after timeout

--- main.py
+++ main.py
@@ -3,11 +3,10 @@ from PyQt5.QtWidgets import QApplication, QWidget, QHBoxLayout, QPushButton
 
 
 async def loop(app, event):
-    app.lastWindowClosed.connect(event.set)
 
     while True:
         app.processEvents()
-        with trio.move_on_after(0.01):
+        with trio.move_on_after(0.00001):
             await event.wait()
         if event.is_set():
             return
@@ -30,6 +29,13 @@ def async_bind(fn):
 
 async def say_hello():
     print('Hello')
+
+
+async def tick_tack(event):
+    while not event.is_set():
+        with trio.move_on_after(0.2):
+            await event.wait()
+        print('Tick tack')
 
 
 async def main():
@@ -58,6 +64,7 @@ async def main():
         global _nursery
         _nursery = nursery
         nursery.start_soon(loop, app, event)
+        nursery.start_soon(tick_tack, event)
 
 
 if __name__ == '__main__':
